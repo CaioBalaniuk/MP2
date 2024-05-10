2e0687b29a0d26b814ba5479e01826dce4ad8ce4
Your Name Comes Here <you@yourdomain.example.com>
2024-05-10 12:55:52 -0300

    teste 24 (com codigo)

--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -29,6 +29,7 @@ TEST_CASE( "Entradas válidas - mas não é resposta pro problema das 8 rainhas.", 
     REQUIRE( problema_8_rainhas("teste_20.txt") == 0 ); // teste para duas rainhas na mesma linha e duas rainhas na mesma coluna
     REQUIRE( problema_8_rainhas("teste_21.txt") == 0 ); // teste para tres rainhas na mesma linha
     REQUIRE( problema_8_rainhas("teste_23.txt") == 0 ); // teste para tres rainhas na mesma coluna
-    REQUIRE( problema_8_rainhas("teste_22.txt") == 0 ); // teste para duas na rainhas na mesma diagonal
+    REQUIRE( problema_8_rainhas("teste_22.txt") == 0 ); // teste para duas rainhas na mesma diagonal
+    REQUIRE( problema_8_rainhas("teste_24.txt") == 0 ); // teste para diversas rainhas na mesma diagonal
 }
 
